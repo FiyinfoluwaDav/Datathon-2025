674c209956a1eb72e9a104aac9b63645a693bf22
Osokoya Fiyinfoluwa <osokoyafiyinfoluwa@gmail.com>
2025-10-12 21:23:52 +0100

    Updated main.py

--- Backend/main.py
+++ Backend/main.py
@@ -29,7 +29,9 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3000",
         "http://127.0.0.1:8000",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://medisense-flax.vercel.app",
+        "https://datafest-backend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
